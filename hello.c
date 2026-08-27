168181cb0e9b91e714f54225dfd075e3317e1b51
Shane Kindred <skindred2@huskers.unl.edu>
2026-08-27 14:28:05 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Shane Kindred
+ *         skindred2@huskers.unl.edu
+ * Date: 2026/8/27
+ *
+ * A simple program that prints out names.
+ *
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+    printf("Shane Kindred\n");
+
+ }
